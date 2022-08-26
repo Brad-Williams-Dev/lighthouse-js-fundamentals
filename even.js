7dd72b39663679d1a4e533f2fb7ede0ad2bbaac1
Brad Williams <bradley_williams2003@hotmail.com>
2022-08-26 07:56:16 -0300

    even.js

---
+++ even.js
@@ -0,0 +1,9 @@
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+/* const fourteenIsEven = isEven(14);
+const sixIsEven = isEven(6); */
+
+
+console.log(isEven(14));
+console.log(isEven(6));
